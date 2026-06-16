cd85532e8b79fcbbc7f2b9a8334702b02d28664a
Sakshi <sakshishenvekar14@gmail.com>
2026-06-16 21:08:06 +0530

    Add runtime

--- app/resources/portfolio.py
+++ app/resources/portfolio.py
@@ -11,13 +11,13 @@ class Portfolio:
 
         self.data = pd.read_csv(file_path)
 
-        self.chroma_client = chromadb.PersistentClient(
-            path="vectorstore"
-        )
+        # self.chroma_client = chromadb.PersistentClient(
+        #     path="vectorstore"
+        # )
 
-        self.collection = self.chroma_client.get_or_create_collection(
-            name="portfolio"
-        )
+        # self.collection = self.chroma_client.get_or_create_collection(
+        #     name="portfolio"
+        # )
 
     def load_portfolio(self): 
 
@@ -45,7 +45,7 @@ class Portfolio:
         results = self.collection.query(
             query_texts=[query],
             n_results=3
-        )
+        ) 
 
         metadata = results.get("metadatas", [])
 
